09cc1051e5544ab9407517f4f0f48512f02b6809
DEVV <david.valencia.valencia@correounivalle.edu.co>
2025-03-01 13:01:37 -0500

    animal interface created and cat class

---
+++ src/main/java/com/example/crapsgame/models/AnimalInterface.java
@@ -0,0 +1,7 @@
+package com.example.crapsgame.models;
+
+public interface AnimalInterface {
+    String name = "animalName";
+
+    void makeSound();
+}
---
+++ src/main/java/com/example/crapsgame/models/Cat.java
@@ -0,0 +1,9 @@
+package com.example.crapsgame.models;
+
+public class Cat implements AnimalInterface{
+    @Override
+    public void makeSound(){
+        System.out.println("Miau miau");
+    }
+
+}
