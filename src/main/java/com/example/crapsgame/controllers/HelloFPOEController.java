88fa53aa6e675b05dde0a4bf94d4f066d2e68f2d
DEVV <david.valencia.valencia@correounivalle.edu.co>
2025-03-08 13:03:07 -0500

    feat: added image to controller

--- src/main/java/com/example/crapsgame/controllers/HelloFPOEController.java
+++ src/main/java/com/example/crapsgame/controllers/HelloFPOEController.java
@@ -4,14 +4,30 @@ import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 
 public class HelloFPOEController {
     @FXML
     private TextField nicknameTextField;
     @FXML
+    private ImageView imageView;
+    private int count;
+
+    @FXML
     public void onActionFPOEButton(ActionEvent actionEvent){
         System.out.println("Hello FPOE from FXML");
+        count += 1;
+        if (count % 2 == 0) {
+            imageView.setImage(new Image(
+                    getClass().getResourceAsStream("com/example/crapsgame/images/casino.png"))
+            );
+        } else {
+            imageView.setImage(new Image(
+                    getClass().getResourceAsStream("com/example/crapsgame/images/something.png"))
+            );
+        }
     }
     @FXML
     public void onKeyPressedNicknameTextField(KeyEvent keyEvent){
